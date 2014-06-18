c292d7e8c68c5f5b9261333a04dbeb8000965928
Wei Gao <jky239@gmail.com>
2014-06-18 10:38:55 +0800

    adjust route

--- index.js
+++ index.js
@@ -1,57 +1,61 @@
 var async = require('async');
 var routington = require('routington');
+var url = require('url');
 
 function Diss() {
   var router = this.router = routington();
-
-  router.use = function(path, middleware) {
-    var nodes = router.define(path);
-    nodes.forEach(function(node) {
-      node.uses = node.uses || [];
-      node.uses.push(middleware);
-    });
-  };
-
-  ['get', 'put', 'post', 'delete', 'head'].forEach(function(method) {
-    var M = method.toUpperCase();
-
-    router[method] = function(path, handler) {
-      var nodes = router.define(path);
-      nodes.forEach(function(node) {
-        node[M] = node[M] || [];
-        node[M].push(handler);
-      });
-    };
-  });
-
-  router.del = router.delete;
 }
 
-Diss.prototype.handle = (req, res, next) {
-  var match = this.router.match(url.parse(req.url).pathname)
-  if (!match)
-    return next();
+Diss.prototype.use = function(path, middleware) {
+  var nodes = this.router.define(path);
+  nodes.forEach(function(node) {
+    node.uses = node.uses || [];
+    node.uses.push(middleware);
+  });
+};
 
-  req.params = match.params;
+['get', 'put', 'post', 'delete', 'head'].forEach(function(method) {
+  var M = method.toUpperCase();
 
-  var node = match.node;
-  var stacks = (node.uses || []).concat(node[req.method] || []);
-
-  if (stacks && stacks.length) {
-    async.series(stacks.map(function(fn) {
-      return function(cb) {
-        try {
-          fn(req, res, cb);
-        } catch (e) {
-          cb(e);
-        }
-      }
-    }), function(err) {
-      next(err);
+  Diss.prototype[method] = function(path, handler) {
+    var nodes = this.router.define(path);
+    nodes.forEach(function(node) {
+      node[M] = node[M] || [];
+      node[M].push(handler);
     });
-  } else {
-    next();
-  }
+  };
+});
+
+Diss.prototype.del = Diss.prototype.delete;
+
+Diss.prototype.routing = function() {
+  var self = this;
+  return function(req, res, next) {
+    var match = self.router.match(url.parse(req.url).pathname);
+    if (!match)
+      return next();
+
+    req.params = match.params;
+
+    var node = match.node;
+    var stacks = (node.uses || []).concat(node[req.method] || []);
+
+    if (stacks && stacks.length) {
+      async.series(stacks.map(function(fn) {
+        return function(cb) {
+          try {
+            fn(req, res, cb);
+          } catch (e) {
+            cb(e);
+          }
+        }
+      }), function(err) {
+        next(err);
+      });
+    } else {
+      next();
+    }
+  };
 };
 
 module.exports = function() {
